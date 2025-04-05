b354fa3253007d0e793850eddc76133164d52848
unknown <purvaspatel1241@gmail.com>
2025-04-05 21:23:49 +0530

    prod-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,17 @@ import DashboardPage from "./dashboard/page";
 export default function Home() {
   return (
     <main className="h-screen p-6">
-      <DashboardPage />
+      < a href="/dashboard" className="text-blue-500 hover:underline">
+    Go to dashboard
+      </a>
+      <br></br>
+      < a href="/register" className="text-blue-500 hover:underline">
+    Create a new account
+      </a>
+      <br></br>
+      < a href="/dashboard" className="text-blue-500 hover:underline">
+    Log in to existing account
+      </a>
     </main>
   );
 }
